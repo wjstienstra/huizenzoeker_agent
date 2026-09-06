f9af0de93a43aa0a7bf14726b34761571e4d44f8
wjstienstra <wjstienstra@gmail.com>
2026-09-06 08:38:42 +0200

    added all brokers back in for final version/production

--- makelaars.py
+++ makelaars.py
@@ -1,19 +1,6 @@
 # makelaars.py
 
 MAKELAARS = [
- 
-    {
-        "naam": "Goos Makelaardij",
-        "base": "https://www.goos.nl",
-        "url": "https://www.goos.nl/woning"
-    }
-]
-
-# Opmerking: Guidan aankoopmakelaar is niet toegevoegd aan de lijst. 
-# Omdat zij alleen als aankoopmakelaar fungeren, hebben zij geen eigen 
-# doorzoekbaar aanbod van te koop staande woningen op hun site.
-
-"""
     {
         "naam": "Rodenburg", 
         "base": "https://rodenburg.nl", 
@@ -63,5 +50,14 @@ MAKELAARS = [
         "naam": "Mulderij Makelaardij",
         "base": "https://www.mulderijmakelaardij.nl",
         "url": "https://www.mulderijmakelaardij.nl/actueel/"
+    }, 
+    {
+        "naam": "Goos Makelaardij",
+        "base": "https://www.goos.nl",
+        "url": "https://www.goos.nl/woning"
     }
-"""
+]
+
+# Opmerking: Guidan aankoopmakelaar is niet toegevoegd aan de lijst. 
+# Omdat zij alleen als aankoopmakelaar fungeren, hebben zij geen eigen 
+# doorzoekbaar aanbod van te koop staande woningen op hun site.
